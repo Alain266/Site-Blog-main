2dd33ed2153e155b22b1e6a49b087643202f24ae
Alain266 <a.okala26@gmail.com>
2023-09-08 21:50:45 +0200

    Acceuil + loading page

---
+++ script.js
@@ -0,0 +1,6 @@
+// Overlay de chargement
+window.addEventListener("load", function() {
+    // Lorsque la page et les ressources sont totalement chargées
+    var loadingOverlay = document.querySelector(".loading-overlay");// Cacher l'overlay de chargement
+    loadingOverlay.style.display = "none"; // Cacher l'overlay de chargement
+});
