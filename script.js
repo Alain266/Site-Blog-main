66f66087edd57a8e7a2cdf34a8f851b1f1217c3f
Alain266 <a.okala26@gmail.com>
2023-09-12 14:35:35 +0200

    debug menu hamburger partie 1

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Overlay de chargement
 window.addEventListener("load", function() {
     // Lorsque la page et les ressources sont totalement chargées
-    var loadingOverlay = document.querySelector(".loading-overlay");// Cacher l'overlay de chargement
+    var loadingOverlay = document.querySelector(".loading-overlay");// Récupérer l'overlay de chargement
     loadingOverlay.style.display = "none"; // Cacher l'overlay de chargement
 });
 
-
+//Afficher le menu hamburger quand on clique sur le bouton
+document.getElementById("menu1").addEventListener("click", function() { 
+    document.getElementById("pagehamburger").style.left = "0";
+});
