8f4fdd5e69b304d2fe7de64b7785d47478659c27
Alain266 <a.okala26@gmail.com>
2023-09-13 13:29:11 +0200

    countdown add

--- script.js
+++ script.js
@@ -34,3 +34,32 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 } 
+
+// Définir la date de fin du compte a rebours
+var countDownDate = new Date("Jan 6, 2024 15:37:25").getTime();
+
+// Mise à jour toute les 1000 millisecondes
+var x = setInterval(function() {
+
+// Obtenir la date et l'heure d'aujourd'hui
+var now = new Date().getTime();
+
+// Trouvez la distance entre maintenant et la date de comptage
+var distance = countDownDate - now;
+
+// Calcul les secondes et minutes 
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Affichez le résultat dans l'élément avec id = "Demo"
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+// Si le décompte est terminé, écrivez du texte
+if (distance < 0) {
+clearInterval(x);
+document.getElementById("demo").innerHTML = "EXPIRED";
+}
+}, 1000);
